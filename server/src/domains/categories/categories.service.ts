6aacc7064f7bfde866526169334926f4f0b7d77f
Samuel123-456 <https://github.com/Samuel123-456/pharma-master-.git>
2025-12-18 20:29:34 +0100

    Category CRUD (version: 0)

--- server/src/domains/categories/categories.service.ts
+++ server/src/domains/categories/categories.service.ts
@@ -1,26 +1,113 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import PrismaService from '@infra/database/prisma.service';
+
+import slugify from 'slugify';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+
+  constructor(private readonly database: PrismaService){}
+
+  async create(dto: CreateCategoryDto) {
+
+    const slug = slugify(dto.title, {
+      lower: true,
+      strict: true,
+    });
+
+    const existingCategory = await this.database.category.findUnique({
+      where: {
+        slug: slug,
+      },
+    });
+
+    if (existingCategory) {
+      throw new BadRequestException('Category with this slug already exists');
+    }
+
+    const categories = await this.database.category.create({
+          data: {
+            title: dto.title,
+            slug: slug,
+            description: dto.description,
+            tags: dto.tags ?? [],
+            color: dto.color,
+            order: dto.order,
+            featured: dto.featured ?? true,
+            coverUrl: dto.coverUrl,
+            isActive: dto.isActive ?? true,
+            stats: dto.stats ?? {},
+          },
+        });
+
+    return  categories;
   }
 
   findAll() {
-    return `This action returns all categories`;
+    return this.database.category.findMany({
+      where: {
+        isActive: true,
+      }
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: number) {
+    const category =  await this.database.category.findUnique({
+      where: {
+        id: id,
+        isActive: true,
+      },
+    });
+    return category;
   }
 
+  async findBySlug(slug: string) {
+    const category = await this.database.category.findUnique({
+      where: {
+        slug: slug,
+        isActive: true,
+      },
+    });
+    return category
+  }
   update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+    const category = this.database.category.update({
+      where: {
+        id: id,
+      },
+      data: {
+        ...updateCategoryDto,
+      },
+    });
+    return category;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: number) {
+    const category = await this.database.category.delete({
+      where: {
+        id: id,
+      },
+    });
+    return category;
   }
+
+  async changeState(id: number) {
+    const category = await this.database.category.findUnique({
+      where: { id },
+    });
+
+    if (!category) {
+      throw new BadRequestException('Category not found');
+    }
+
+    const updatedCategory = await this.database.category.update({
+      where: { id },
+      data: { isActive: !category.isActive },
+    });
+
+    return updatedCategory;
+  }
+
 }
--- server/src/domains/services/dto/create-service.dto.ts
+++ server/src/domains/services/dto/create-service.dto.ts
@@ -1,1 +1,4 @@
-export class CreateServiceDto {}
+
+export class CreateServiceDto {
+    
+}
