0530ac7d9320353e9e7da8757762fbe635ba3234
Francisco Diakomas <franciscodiakoma@gmail.com>
2025-12-20 20:01:08 +0100

    feat : Slug Service

--- server/src/domains/categories/features/v1/categories.controller.ts
+++ server/src/domains/categories/features/v1/categories.controller.ts
@@ -7,15 +7,18 @@ import {
   Param,
   Delete,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from '../../dto/create-category.dto';
 import { UpdateCategoryDto } from '../../dto/update-category.dto';
+import { IsAdminGuard } from '@core/http/guards/isAdmin.guard';
 
 @Controller('v1/categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
+  @UseGuards(IsAdminGuard)
   @Post()
   async create(@Body() createCategoryDto: CreateCategoryDto) {
     return await this.categoriesService.create(createCategoryDto);
@@ -33,6 +36,8 @@ export class CategoriesController {
   findBySlug(@Param('slug') slug: string) {
     return this.categoriesService.findBySlug(slug);
   }
+
+  @UseGuards(IsAdminGuard)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -40,10 +45,14 @@ export class CategoriesController {
   ) {
     return this.categoriesService.update(+id, updateCategoryDto);
   }
+
+  @UseGuards(IsAdminGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.categoriesService.remove(+id);
   }
+
+  @UseGuards(IsAdminGuard)
   @Put('change-state/:id')
   changeState(@Param('id') id: string) {
     return this.categoriesService.changeState(+id);
