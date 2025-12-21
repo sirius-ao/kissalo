ca8a498e417717518479bc47da73108a7be17f4b
Francisco Diakomas <franciscodiakoma@gmail.com>
2025-12-21 13:38:12 +0100

    feat : conlits

--- client/.next/dev/static/chunks/app_layout_tsx_eab4c14e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__28bc9c2a._.css",
-  "static/chunks/_67a9e968._.js"
-],
-    source: "dynamic"
-});
